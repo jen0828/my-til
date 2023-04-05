b7e65d2f83e9184c6c8599a6a42aff5048671310
Jenny Moon <jenny0moon@gmail.com>
2023-04-05 14:14:43 +0100

    Update font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <h1 className="bg-gray-500 text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <h1 className="bg-gray-500 text-3xl font-bold underline">Hello world!</h1>
+  );
 }
