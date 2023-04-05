87da8cf984f123fa77e9a75525ef43e919d21c27
Jenny Moon <jenny0moon@gmail.com>
2023-04-05 15:44:31 +0100

    Add styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <section className="bg-gray-500 text-3xl font-bold underline">Hello world!</section>
+    <section className="bg-gray-500">Main</section>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
 export default function Footer() {
-  return <p>© 2023-present Jenny Moon. All Rights Reserved.</p>
+  return <p className='bg-slate-900 text-white py-2 text-sm text-center'>
+    © 2023-present Jenny Moon. All Rights Reserved.</p>
 }
