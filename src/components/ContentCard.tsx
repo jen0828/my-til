0e93456003a2811da05cad68a0a3a6818f59de42
Jenny Moon <jenny0moon@gmail.com>
2023-04-06 16:50:14 +0100

    Create new component - ContentCard

---
+++ src/components/ContentCard.tsx
@@ -0,0 +1,25 @@
+import { Content } from '@/service/contents';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type Props = { content: Content };
+export default function ContentCard({
+  content: { title, description, date, category, path },
+}: Props) {
+  return (
+    <Link href={`/posts/${path}`}>
+      <Image
+        src={`/images/contents/${path}.png`}
+        alt={title}
+        width={300}
+        height={200}
+      />
+      <div>
+        <time>{date.toString()}</time>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <span>{category}</span>
+      </div>
+    </Link>
+  );
+}
--- src/components/ContentsGrid.tsx
+++ src/components/ContentsGrid.tsx
@@ -1,11 +1,12 @@
 import { Content } from '@/service/contents';
+import ContentCard from './ContentCard';
 
 type Props = { contents: Content[] };
 export default function ContentsGrid({ contents }: Props) {
   return (
     <ul>
       {contents.map((content) => (
-        <li key={content.path}>{content.title}</li>
+        <li key={content.path}><ContentCard content={content} /></li>
       ))}
     </ul>
   );
