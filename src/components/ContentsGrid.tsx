86bd47537c61133887f2bfa9fedac467b813b677
Jenny Moon <jenny0moon@gmail.com>
2023-04-06 17:39:33 +0100

    Add styling

--- src/components/ContentsGrid.tsx
+++ src/components/ContentsGrid.tsx
@@ -4,9 +4,11 @@ import ContentCard from './ContentCard';
 type Props = { contents: Content[] };
 export default function ContentsGrid({ contents }: Props) {
   return (
-    <ul>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {contents.map((content) => (
-        <li key={content.path}><ContentCard content={content} /></li>
+        <li key={content.path}>
+          <ContentCard content={content} />
+        </li>
       ))}
     </ul>
   );
