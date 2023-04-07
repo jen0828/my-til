1824fff9ba0486893902e1540aa401c587185551
Jenny Moon <jenny0moon@gmail.com>
2023-04-07 10:48:30 +0100

    Set up contents page

--- src/app/contents/page.tsx
+++ src/app/contents/page.tsx
@@ -1,3 +1,7 @@
-export default function ContentsPage() {
-  return <p>Contents Page</p>;
+import { getAllContents } from "@/service/contents";
+
+export default async function ContentsPage() {
+  const contents = await getAllContents()
+  const categories = [...new Set(contents.map((content) => content.category))];
+  return <p>contents</p>
 }
