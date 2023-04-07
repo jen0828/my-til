a5ad7eec29e3034ec3fadca82690bd755384ff14
Jenny Moon <jenny0moon@gmail.com>
2023-04-07 20:52:09 +0100

    Add tailwind prose option

--- src/components/MarkDownViewer.tsx
+++ src/components/MarkDownViewer.tsx
@@ -4,6 +4,8 @@ import remarkGfm from 'remark-gfm'
 
 export default function MarkdownViewer({texts}: {texts: string}) {
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]}>{texts}</ReactMarkdown>
+    <ReactMarkdown
+    className='prose lg:prose-xl'
+    remarkPlugins={[remarkGfm]}>{texts}</ReactMarkdown>
   )
 }
