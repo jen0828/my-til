145ff8ffa3d4784c71737bbc98de56a11d3aadee
Jenny Moon <jenny0moon@gmail.com>
2023-04-08 09:38:34 +0100

    Update styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
     <>
       <Profile />
       <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
-        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <h2 className={TITLE_CLASS}>About Me</h2>
         <p>
           I am a passionate Software Engineer who values writing quality, fully
           tested, extensible code. <br />
