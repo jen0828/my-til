5c6136e44ee11d302f696b5e5f530d33388c171f
Jenny Moon <jenny0moon@gmail.com>
2023-04-08 08:38:12 +0100

    Add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,29 @@
+import { AiFillGithub, AiFillLinkedin, AiFillIdcard } from 'react-icons/ai';
+
+const LINKS = [
+  { icon: <AiFillGithub />, url: '' },
+  { icon: <AiFillLinkedin />, url: '' },
+  { icon: <AiFillIdcard />, url: '' },
+];
+
 export default function ContactPage() {
-  return <p>Contact Page</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">{" Let's Connect!"}</h2>
+      <br></br>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400">
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+    </section>
+  );
 }
