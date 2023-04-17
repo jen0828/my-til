b0ba24d2ad93ef9224561ac2401c6fa76e6e122a
Jenny Moon <jenny0moon@gmail.com>
2023-04-17 10:07:31 +0100

    Update styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,25 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='flex justify-between items-center p-4'>
+    <header className="flex flex-col sm:flex-row justify-between items-center p-4">
       <Link href="/">
-        <h1 className='text-3xl font-bold'>{"Jenny's TIL"}</h1>
+        <h1 className="text-3xl font-bold mb-2 sm:mb-0 sm:text-4xl">
+          {"Jenny's TIL"}
+        </h1>
       </Link>
-      <nav className='flex gap-4'>
-        <Link href="/">home</Link>
-        <Link href="/about">about</Link>
-        <Link href="/contents">contents</Link>
-        <Link href="/contact">contact</Link>
+      <nav className="flex flex-wrap sm:flex-nowrap text-sm sm:text-base p-2">
+        <Link href="/" className="mx-2">
+          home
+        </Link>
+        <Link href="/about" className="mx-2">
+          about
+        </Link>
+        <Link href="/contents" className="mx-2">
+          contents
+        </Link>
+        <Link href="/contact" className="mx-2">
+          contact
+        </Link>
       </nav>
     </header>
   );
